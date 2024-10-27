6d152668f14c5c3c836793f42b7a7729febd59d8
Ruslan <ruslan@example.com>
2024-10-27 11:11:58 +0500

    upd

--- src/modules/config/buildProps.groovy
+++ src/modules/config/buildProps.groovy
@@ -8,7 +8,7 @@ class buildProps {
     def getConfig() {
         if (this.projectBuildProps == null) {
             Yaml yaml = new Yaml()
-            projectBuildProps = yaml.load(('config\\build-props.yaml'as File).text)
+            projectBuildProps = yaml.load(('.\\config\\build-props.yaml'as File).text)
         }
 //        else {
 //            throw new Exception("ProjectBuildProps file not found or is empty.")
