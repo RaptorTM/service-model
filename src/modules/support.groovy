6e9e90a9c7ca0f5130cec0eb485b7fc9700672e3
Ruslan <ruslan@example.com>
2024-10-27 19:03:51 +0500

    upd

--- src/modules/support.groovy
+++ src/modules/support.groovy
@@ -8,6 +8,26 @@ class support {
         Path path = servicePath
         return Files.exists(path)
     }
+
+    static def load_sm_classes() {
+        // Загружаем все необходимые файлы
+        load 'src/ci/Props.groovy'
+        load 'src/ci/allure.groovy'
+        load 'src/info/ServiceInfo.groovy'
+        load 'src/info/ClusterInfo.groovy'
+        load 'src/info/LandscapeInfo.groovy'
+        load 'src/info/DeployEnvironmentInfo.groovy'
+        load 'src/modules/config/serviceModel.groovy'
+        load 'src/modules/config/commonBuildProps.groovy'
+        load 'src/modules/config/buildProps.groovy'
+        load 'src/modules/build.groovy'
+        load 'src/modules/dotnet.groovy'
+        load 'src/modules/frontend.groovy'
+        load 'src/modules/infrastructure.groovy'
+        load 'src/modules/migrations.groovy'
+        load 'src/modules/python.groovy'
+        load 'src/modules/shellResult.groovy'
+    }
 }
 
 
