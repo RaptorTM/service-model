2e0a9d99db0491c026b612fc4f619e46b14a265e
Ruslan <ruslan@example.com>
2024-10-27 19:50:33 +0500

    upd

--- src/class_loader.groovy
+++ src/class_loader.groovy
@@ -1,7 +1,6 @@
 def load_classes() {
     // Загружаем все необходимые файлы
     load 'src/ci/Props.groovy'
-    load 'src/ci/allure.groovy'
     load 'src/info/ServiceInfo.groovy'
     load 'src/info/ClusterInfo.groovy'
     load 'src/info/LandscapeInfo.groovy'
@@ -10,12 +9,6 @@ def load_classes() {
     load 'src/modules/config/commonBuildProps.groovy'
     load 'src/modules/config/buildProps.groovy'
     load 'src/modules/build.groovy'
-    load 'src/modules/dotnet.groovy'
-    load 'src/modules/frontend.groovy'
-    load 'src/modules/infrastructure.groovy'
-    load 'src/modules/migrations.groovy'
-    load 'src/modules/python.groovy'
-    load 'src/modules/shellResult.groovy'
 }
 
 return this //!!!!!!!!!_____Закоментировать на время отладки в IDE_____!!!!!!!!!
