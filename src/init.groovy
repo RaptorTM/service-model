a74101ba76d4d99c838c71d0371bcaef8be65d36
ruslan <test@mai.ru>
2024-10-31 20:35:33 +0500

    upd

--- src/init.groovy
+++ src/init.groovy
@@ -1,11 +1,11 @@
-//import info.ServiceInfo
+import info.ServiceInfo
 import info.ClusterInfo
 import info.LandscapeInfo
 import info.DeployEnvironmentInfo
 import modules.*
 import modules.config.buildProps
 import modules.config.commonBuildProps
-//import modules.config.serviceModel
+import modules.config.serviceModel
 
 class init {
     void get(){
