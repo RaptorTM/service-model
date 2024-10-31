dc5cb76fcdcd73b096d10a0d7b45a36e66abe676
ruslan <test@mai.ru>
2024-10-31 20:37:19 +0500

    upd

--- src/init.groovy
+++ src/init.groovy
@@ -1,8 +1,10 @@
+import info.*
 import info.ServiceInfo
 import info.ClusterInfo
 import info.LandscapeInfo
 import info.DeployEnvironmentInfo
 import modules.*
+import modules.config.*
 import modules.config.buildProps
 import modules.config.commonBuildProps
 import modules.config.serviceModel
