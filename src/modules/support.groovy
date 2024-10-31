a482678a3988ca17a41e1a2e780d03c5210ac809
ruslan <test@mai.ru>
2024-10-31 21:16:17 +0500

    upd

--- src/modules/support.groovy
+++ src/modules/support.groovy
@@ -13,11 +13,11 @@ def load_sm_classes() {
     try {
         load 'src/ci/Props.groovy'
         load 'src/ci/allure.groovy'
-//        load 'src/info/ServiceInfo.groovy'
+        load 'src/info/ServiceInfo.groovy'
         load 'src/info/ClusterInfo.groovy'
         load 'src/info/LandscapeInfo.groovy'
         load 'src/info/DeployEnvironmentInfo.groovy'
-//        load 'src/modules/config/serviceModel.groovy'
+        load 'src/modules/config/serviceModel.groovy'
         load 'src/modules/config/commonBuildProps.groovy'
         load 'src/modules/config/buildProps.groovy'
         load 'src/modules/build.groovy'
